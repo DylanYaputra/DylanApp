e1dcea3adea023319f1cbc1e00247c8df1f0df3b
dylanyaputra <dylan22071975@gmail.com>
2025-06-01 22:00:52 +0700

    1 June 2025 1st Commit

--- app/src/main/java/com/apppertama/dylanapp/DylanActivity.kt
+++ app/src/main/java/com/apppertama/dylanapp/DylanActivity.kt
@@ -1,23 +1,20 @@
 package com.apppertama.dylanapp
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class DylanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_dylan)
-
-        val inputNama = findViewById<EditText>(R.id.inputNama)
-        val buttonTampil = findViewById<Button>(R.id.buttonTampil)
-
-        buttonTampil.setOnClickListener {
-            val nama = inputNama.text.toString()
-            val pesan = if (nama.isNotEmpty()) "Halo, $nama!" else "Silakan isi nama terlebih dahulu"
-            Toast.makeText(this, pesan, Toast.LENGTH_SHORT).show()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
 }
